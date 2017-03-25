7115b436381cb9d1249e3afd09b8d4f677990de4
jackhsujungtse <jackhsujungtse@gmail.com>
2017-03-25 23:38:10 +0800

    made a new page name reason

--- HW4/Work/app.js
+++ HW4/Work/app.js
@@ -8,11 +8,12 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname+'/test.html');
 });
 
+app.get('/reason',function(req,res){
+       res.end('<html><head></head><body><h1>Hello, this project was a simple project made for my friend Daniel years before, hopefully to update it sometime~ XDXD</h1></body></html>');
+});
 app.get('/404', function(req, res) {
     res.writeHead(404);
     res.end('<html><head></head><body><h1>Sorry, page not found!!! XDXD</h1></body></html>');
-    //res.end();
-    //res.json({ firstname: 'John', lastname: 'Doe' });
 });
 
 app.listen(port);
