99050d15fa44e732ec2d9149832acb59c607ee88
jackhsujungtse <jackhsujungtse@gmail.com>
2017-03-26 00:04:10 +0800

    edited reason for new picture and some inline codes

--- HW4/Work/app.js
+++ HW4/Work/app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/reason',function(req,res){
-       res.end('<html><head></head><body><h1>Hello, this project was a simple project made for my friend Daniel years before, hopefully to update it sometime~ XDXD</h1></body></html>');
+       res.end('<html><head></head><body><h2>Hello, this project was a simple project made for my friend Daniel years before, hopefully to update it sometime~ XDXD</h2></body><p></p><h2>This is a testing picture called...</h2><p></p><h1 style = "color : red">MILKY GUAVA!!</h1><img src="https://raw.githubusercontent.com/jackhsujungtse/MERN-HW/master/HW4/Work/milkyguava.JPG" width = 50% alt = "milky guava"/></html>');
 });
 app.get('/404', function(req, res) {
     res.writeHead(404);
